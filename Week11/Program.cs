14c012da9d866cac24e5306194b1b720ac20ea73
hardikshah18 <hshah366@gmail.com>
2024-03-30 10:01:55 -0400

    DotNet_Lab7

---
+++ Week11/Program.cs
@@ -0,0 +1,16 @@
+﻿using Week11.Models.Entities;
+
+
+var dbContext = new MMABooksContext();
+
+var customers = dbContext.Customers;
+
+Console.WriteLine("Customer Names:\n");
+
+foreach (var customer in customers)
+
+{
+
+    Console.WriteLine(customer.Name);
+
+}
